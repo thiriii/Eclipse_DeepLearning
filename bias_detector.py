650c310698d0d2cb3e78641f61db5ab267e0626c
Thwun Thiri Thu <145767383+thiriii@users.noreply.github.com>
2025-03-02 02:06:18 +0800

    Delete bias_detector.py

--- bias_detector.py
+++
@@ -1,7 +0,0 @@
-from transformers import pipeline
-
-def detect_bias(text):
-    classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
-    labels = ["left-wing bias", "right-wing bias", "neutral"]
-    results = classifier(text, labels)
-    return {"bias_analysis": results}
