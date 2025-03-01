5067fa45d1732aceba0e39964645b49af1908c82
Thwun Thiri Thu <145767383+thiriii@users.noreply.github.com>
2025-03-02 02:06:46 +0800

    Delete misinformation_checker.py

--- misinformation_checker.py
+++
@@ -1,8 +0,0 @@
-from transformers import pipeline
-import requests
-
-def check_fake_news(article_text):
-    model = pipeline("text-classification", model="microsoft/deberta-v3-large-mnli")
-    labels = ["real", "fake"]
-    result = model(article_text, labels)
-    return {"fake_news_score": result}
